3f31c77cedc46be5859fc82bdbc56fa83306ca73
Terry Zhang <zhangtianyu@buaa.edu.cn>
2023-01-29 19:17:19 +0800

    fix bug in tencent mail

--- server_shutdown.py
+++ server_shutdown.py
@@ -7,7 +7,9 @@ if __name__ == '__main__':
     reconnect_timeout = 600
     application = tornado.web.Application([
         ('/', webio_handler(page_shutdown, cdn=True,reconnect_timeout=reconnect_timeout)),
-  
+        ('/main', webio_handler(page_shutdown, cdn=True,reconnect_timeout=reconnect_timeout)),
+        ('/gallery', webio_handler(page_shutdown, cdn=True,reconnect_timeout=reconnect_timeout)),
+
     ])
     application.listen(port=5001, address='localhost')
     tornado.ioloop.IOLoop.current().start()
