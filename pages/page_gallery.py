5f616891bcc2c42898117db27816bc17b64f07f2
Terry Zhang <zhangtianyu@buaa.edu.cn>
2023-02-04 09:30:54 +0800

    change to local

--- pages/page_gallery.py
+++ pages/page_gallery.py
@@ -107,7 +107,6 @@ def load_more_images_on_gallery(val=None):
             images.append(session.local.image_list.pop(0))
     # add images to the shortest col
     if len(images) > 0:
-        arr = []
         for img_info in images:
 
             img_url = img_info["image"]
@@ -119,6 +118,7 @@ def load_more_images_on_gallery(val=None):
             short_ind = np.argmin(session.local.col_height)
             session.local.col_height[short_ind] += height * (256 / width)
             with use_scope("img-col"+str(short_ind)):
+                #img_url_md = img_url.replace(".jpeg",".md.jpeg")
                 put_image(img_url).onclick(
                     partial(
                         show_image_information_window, 
