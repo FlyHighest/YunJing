619cd6db6c02f74345e6c22d386c256579b6ae5b
Terry Zhang <zhangtianyu@buaa.edu.cn>
2023-02-15 22:55:43 +0800

    add none for template

--- pages/page_main.py
+++ pages/page_main.py
@@ -82,9 +82,12 @@ def show_image_information_window(img_url,genid, fuke_func=None):
 
 
 def fill_prompt_template(template_key):
-    fill_prompt, fill_neg_prompt = prompt_template[template_key]
-    pin['prompt'] = fill_prompt
-    pin['negative_prompt'] = fill_neg_prompt
+    if template_key=="请选择模版...":
+        return 
+    else:
+        fill_prompt, fill_neg_prompt = prompt_template[template_key]
+        pin['prompt'] = fill_prompt
+        pin['negative_prompt'] = fill_neg_prompt
 
 def del_from_history(genid):
     session.local.rclient.del_history(session.local.client_id,genid)
