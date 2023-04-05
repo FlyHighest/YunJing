4355276c877659944fc4f33f90c7da1ecc30d84d
Terry Zhang <zhangtianyu@buaa.edu.cn>
2023-04-05 11:55:00 +0800

    favicon

--- server.py
+++ server.py
@@ -9,12 +9,14 @@ if __name__ == '__main__':
     reconnect_timeout = 600
     cdn = "/statics/html"
     application = tornado.web.Application([
+        (r'/favicon.ico()', tornado.web.StaticFileHandler,{"path":os.path.join(os.path.dirname(__file__),"statics/favicon.ico")}),
         ('/', webio_handler(page_index, cdn=cdn,reconnect_timeout=reconnect_timeout)),
         ('/main', webio_handler(page_main, cdn=cdn,reconnect_timeout=reconnect_timeout)),
         ('/gallery', webio_handler(page_gallery, cdn=cdn,reconnect_timeout=reconnect_timeout)),
         ('/help', webio_handler(page_help, cdn=cdn,reconnect_timeout=reconnect_timeout)),
         ('/account', webio_handler(page_account, cdn=cdn,reconnect_timeout=reconnect_timeout)),
-        (r'^/statics/(.*)$', tornado.web.StaticFileHandler,{"path":os.path.join(os.path.dirname(__file__),"statics")})
+        (r'^/statics/(.*)$', tornado.web.StaticFileHandler,{"path":os.path.join(os.path.dirname(__file__),"statics")}),
+        
     ])
     application.listen(port=server_port, address='localhost')
     tornado.ioloop.IOLoop.current().start()
