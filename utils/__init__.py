4228eb63a48a90ad6c0006b1983182ec780c894a
Terry Zhang <zhangtianyu@buaa.edu.cn>
2023-05-28 17:34:16 +0800

    update database

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,7 @@
 from .utils import  get_username,put_column_autosize,put_file_upload,put_row_autosize
 from .constants import *
 from  .custom_exception import QueueTooLong,NSFWDetected,ServerError,TooFrequent
-from .remote_tasks import task_post_enhance_prompt,task_post_image_gen,task_post_upscale,task_publish_to_gallery
+from .remote_tasks import task_post_enhance_prompt,task_post_upscale,task_publish_to_gallery
 from .mail_service import send_verification_mail
 
 from .storage_tool import upload_to_storage,get_presigned_url_tencent
