135ce8fef52ba0bd7e9964973e84db3094c78a65
Terry Zhang <tyz.xyz@qq.com>
2023-09-10 22:36:16 +0800

    init

--- server_tools.py
+++ server_tools.py
@@ -7,7 +7,6 @@ r = redis.Redis(decode_responses=True)
 
 def find_users_activate(date):
     all_history = r.keys("history:*")
-    user_num = len(all_history)
     users_activate = set()
     for history_key in tqdm(all_history):
         url_and_genids = [json.loads(i) for i in r.lrange(history_key,0,-1)]
@@ -19,15 +18,14 @@ def find_users_activate(date):
             if gen_time.split(" ")[0]==date:
                 users_activate.add(history_key.split(":")[1])
                 break
-    # print("用户总数:",user_num)
-    # print(f"活跃用户数({date}): {len(users_activate)}")
-    return user_num, len(users_activate)
+
+    return  len(users_activate)
 
 def report_last_days(days):
     for i in range(days):
         date = (datetime.datetime.now() - datetime.timedelta(days=i)).strftime("%Y-%m-%d")
-        user_num, user_activate = find_users_activate(date)
-        print(f"{date} 活跃用户数: {user_activate}; 用户总数: {user_num}")
+        user_activate = find_users_activate(date)
+        print(f"{date} 活跃用户数: {user_activate}")
 
 if __name__ == "__main__":
     report_last_days(7)
