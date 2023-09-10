6303a8a646420eab84aa269d0de5af5b265c96fa
Terry Zhang <tyz.xyz@qq.com>
2023-09-10 22:34:36 +0800

    init

--- server_tools.py
+++ server_tools.py
@@ -2,18 +2,20 @@ import redis
 from IPython import embed 
 import json 
 import datetime 
+from tqdm import tqdm 
 r = redis.Redis(decode_responses=True)
 
 def find_users_activate(date):
     all_history = r.keys("history:*")
     user_num = len(all_history)
-    users_activate = []
-    for history_key in all_history:
-        url,genid = [json.loads(i) for i in r.lrange(history_key,0,-1)]
-        gen_time = r.hget(f"image:{genid}","gentime")
-        if gen_time.split(" ")[0]==date:
-            users_activate.append(history_key.split(":")[1])
-            break 
+    users_activate = set()
+    for history_key in tqdm(all_history):
+        url_and_genids = [json.loads(i) for i in r.lrange(history_key,0,-1)]
+        for url,genid in url_and_genids:
+            gen_time = r.hget(f"image:{genid}","gentime")
+            if gen_time.split(" ")[0]==date:
+                users_activate.add(history_key.split(":")[1])
+                break
     # print("用户总数:",user_num)
     # print(f"活跃用户数({date}): {len(users_activate)}")
     return user_num, len(users_activate)
